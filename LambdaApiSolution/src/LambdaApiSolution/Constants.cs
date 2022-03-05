954187f1a9291b66fe2537eabb74c732869f4063
srinisanthanam-energism <srini.santhanam@energism.info>
2022-03-05 12:30:08 -0600

    sd

--- LambdaApiSolution/src/LambdaApiSolution/Constants.cs
+++ LambdaApiSolution/src/LambdaApiSolution/Constants.cs
@@ -3,8 +3,8 @@ namespace LambdaApiSolution
 	public static class Constants
 	{
 		public static string Branch = "main";
-		public static string GitHubTokenSecretsManagerId = "GitHub-Token";
-		public static string Owner = "scottenriquez";
+		public static string GitHubTokenSecretsManagerId = "ei/txrrc/github";
+		public static string Owner = "SriniSanthanam-energism";
 		public static string RepositoryName = "dotnet-5-lambda-api-cdk";
 	}
 }
